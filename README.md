2655868a8568306e741ff48813e2c7ae65fb327a
mastermaiknemo <kwidzinskimichael@gmail.com>
2016-08-09 22:43:18 +0200

    starter commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Delightfull WikipediaMapper.
+
+all you need to do: install the missing gems and run 'ruby rubapi.rb'.
+Then visit localhost:4567 and enjoy the enter key (dont keep it pressed too long).
+ *__*
+
+
+
---
+++ rubapi.rb
@@ -0,0 +1,37 @@
+# author mastermaiknemo github
+require 'sinatra'
+require 'sinatra/reloader'
+require 'haml'
+require 'json'
+require 'mechanize'
+
+get '/' do 
+	$counter = 1
+	haml :layout
+end
+
+get '/pagein' do 
+	$title = params['val']
+	a = Mechanize.new
+	req1 = a.get "https://en.wikipedia.org/wiki/#{$title}"
+	puts $title
+	$nodes1 = req1.parser.xpath "/html/body/div[3]/div[3]/div[4]/p[1]"
+	links = []
+	$nodes1.xpath("a/@href").each do |l|
+		links.push l.to_s.gsub(/^\/wiki\//, '')
+	end
+	$backlinks = links
+	content_type :json
+	{:backlinks => $backlinks}.to_json
+end
+
+get '/getnodes' do
+	nodes = []
+	nodes.push ({:id => $counter.to_s, :label => $title})
+	$backlinks.each do |b|
+		$counter += 1
+		nodes.push ({:id => $counter.to_s, :label => b})
+	end
+	$counter += 1
+	nodes.to_json
+end
